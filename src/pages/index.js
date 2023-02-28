8ff0b298fff8d9fae91db2bd54affe7a9a7fae90
Christian Anca <christiananca@Christians-MacBook-Pro.local>
2023-02-28 15:55:22 -0500

    banner style done, began product feed component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,28 @@
 import Head from "next/head";
+import Banner from "../components/Banner"
 import Header from "../components/header";
+import ProductFeed from "../components/ProductFeed";
 
 export default function Home() {
   return (
-    <div>
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon 2.0</title>
       </Head>
 
       
       
-        {/* header */}
+        {/* Header */}
         <Header />
+
+        <main className="max-w-screen-2xl mx-auto">
+          {/* Banner */}
+          <Banner />
+
+
+          {/* ProductFeed */}
+          <ProductFeed />
+        </main>
 
     </div>
   );

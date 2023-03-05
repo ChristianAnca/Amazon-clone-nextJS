4052b11b81b21ab9ea6a5678533e139de781d811
Christian Anca <christiananca@Christians-MacBook-Pro.local>
2023-03-04 19:20:39 -0500

    api/auth started and fixed hydration issue

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,16 +3,27 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { StarIcon } from '@heroicons/react/solid';
 import numeral from 'numeral';
+import { useEffect } from 'react';
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
 function Product({ id, title, price, description, category, image }) {
-const [rating] = useState(
-    Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
-);
+    const [rating, setRating] = useState(1);
 
-const [hasPrime] = useState(Math.random() < 0.5)
+    const [hasPrime, setHasPrime] = useState(true);
+  
+    useEffect(() => {
+      setRating(
+        Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+      );
+      setHasPrime(Math.random() < 0.5);
+    }, []);
+// const [rating] = useState(
+//     Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
+// );
+
+// const [hasPrime] = useState(Math.random() < 0.5)
 
   return (
     <div className='relative flex flex-col m-5 bg-white z-30 p-10'>
